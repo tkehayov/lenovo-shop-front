ccb700b68d9ac77290c6e4e33e4000dc48d436fe
kehayov <tihomirkehayov@deltasource.eu>
2017-08-20 13:10:31 +0300

    remove unnecessary scripts from contact page

--- product.js
+++ product.js
@@ -1,18 +1,11 @@
-// Vue.http.options.emulateJSON = true; // send as 
 new Vue({
     el: '#shopping-cart',
-    data: {
-        ID: "123",
-        ProductName: "product",
-        Quantity: 1
-    },
     methods: {
       addCart: function() {
         id = new URLSearchParams(window.location.search).get("id");
         this.$http.post('/cart',{
-            ID: id,
-            ProductName: "product",
-            Quantity: 1
+            id: id,
+            quantity: 1
         }).then(function(data) {
 
             console.log("success");
