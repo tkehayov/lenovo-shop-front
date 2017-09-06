de916dc3db443590a8fd148fe4083d0de4aca1f9
kehayov <tihomirkehayov@deltasource.eu>
2017-09-06 20:30:12 +0300

    restart page after successfully removing product from product cart

--- shopping-cart.js
+++ shopping-cart.js
@@ -8,18 +8,27 @@ new Vue({
 	        }
 	    }
     },
-	methods:{ 
+	methods:{
 		getCart: function(){
 	  		 this.$http.get('/cart').then(response=>{
-	  		 	if(response.body){
-	           this.carts = response.body;
-	           console.log(this.carts.overallPrice)
+	  		   if(response.body){
+	           	this.carts = response.body;
 	           }
-	           if(this.carts.overallPrice!=undefined){	
+
+	           if(this.carts.overallPrice!=undefined){
 	           	this.carts.overallPrice = Number((this.carts.overallPrice).toFixed(2));
 	           }
 	           return this.carts;
 	      });
+	  	},
+
+	  	removeCart: function(id){
+
+	  		this.$http.delete('/cart/'+id).then(response=>{
+	  			location.reload();
+	  		},function(error){
+	  			console.log(error);
+	  		});
 	  	}
   },
   	mounted: function () {
